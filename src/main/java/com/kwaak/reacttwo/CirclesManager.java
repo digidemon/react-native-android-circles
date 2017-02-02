1efdb9f4b6249e967278033bd4f19b5253041874
esaltaferi <esaltaferi@digidemon.eu>
2017-02-02 12:02:04 +0200

    compatible with react native 0.23

--- src/main/java/com/kwaak/reacttwo/CirclesManager.java
+++ src/main/java/com/kwaak/reacttwo/CirclesManager.java
@@ -8,7 +8,7 @@ import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.ReadableArray;
 import com.facebook.react.bridge.WritableMap;
 import com.facebook.react.common.MapBuilder;
-import com.facebook.react.uimanager.ReactProp;
+import com.facebook.react.uimanager.annotations.ReactProp;
 import com.facebook.react.uimanager.SimpleViewManager;
 import com.facebook.react.uimanager.ThemedReactContext;
 import com.facebook.react.uimanager.events.RCTEventEmitter;
@@ -18,7 +18,7 @@ import java.util.Map;
 import at.grabner.circleprogress.CircleProgressView;
 import at.grabner.circleprogress.TextMode;
 
-public class CirclesManager extends SimpleViewManager<CirclesView> implements CircleProgressView.OnProgressChangedListener {
+public class CirclesManager extends SimpleViewManager <CircleProgressView> implements CircleProgressView.OnProgressChangedListener {
 
     public static final int COMMAND_SPIN = 1;
     public static final int COMMAND_STOPSPINNING = 2;
@@ -26,7 +26,7 @@ public class CirclesManager extends SimpleViewManager<CirclesView> implements Ci
     private boolean _animated;
     private float value;
     private ThemedReactContext context;
-    private  CirclesView view;
+    private  CircleProgressView view;
     public CirclesManager() {
     }
 
@@ -40,16 +40,16 @@ public class CirclesManager extends SimpleViewManager<CirclesView> implements Ci
     }
 
     @Override
-    public CirclesView createViewInstance(ThemedReactContext context) {
+    public CircleProgressView createViewInstance(ThemedReactContext context) {
         this.context = context;
-        CirclesView view =  new CirclesView(context);
+        CircleProgressView view = new CircleProgressView(context, null);
         this.view = view;
         view.setOnProgressChangedListener(this);
         return view;
     }
 
     @ReactProp(name = "barColors")
-    public void setBarColors(CirclesView view, ReadableArray val) {
+    public void setBarColors(CircleProgressView view, ReadableArray val) {
         int[] vals = new int[val.size()];
         for(int i = 0; i < val.size(); i++) {
             vals[i] = Color.parseColor(val.getString(i));
@@ -58,50 +58,50 @@ public class CirclesManager extends SimpleViewManager<CirclesView> implements Ci
     }
 
     @ReactProp(name = "barWidth", defaultInt = 0)
-    public void setBarWidth(CirclesView view, int val) {
+    public void setBarWidth(CircleProgressView view, int val) {
         view.setBarWidth(val);
     }
 
     @ReactProp(name = "blockCount", defaultInt = 0)
-    public void setBlockCount(CirclesView view, int val) {
+    public void setBlockCount(CircleProgressView view, int val) {
         view.setBlockCount(val);
     }
     @ReactProp(name = "blockScale", defaultFloat = 0)
-    public void setBlockScale(CirclesView view, float val) {
+    public void setBlockScale(CircleProgressView view, float val) {
         view.setBlockScale(val);
     }
 
     @ReactProp(name = "contourColor")
-    public void setContourColor(CirclesView view, @Nullable String val) {
+    public void setContourColor(CircleProgressView view, @Nullable String val) {
         view.setContourColor(Color.parseColor(val));
     }
 
     @ReactProp(name = "textColor")
-    public void setTextColor(CirclesView view, @Nullable String val) {
+    public void setTextColor(CircleProgressView view, @Nullable String val) {
         view.setTextColor(Color.parseColor(val));
     }
     @ReactProp(name = "contourSize", defaultFloat = 0)
-    public void setContourSize(CirclesView view, float val) {
+    public void setContourSize(CircleProgressView view, float val) {
         view.setContourSize(val);
     }
 
     @ReactProp(name = "delayMillis", defaultInt = 0)
-    public void setDelayMillis(CirclesView view, int val) {
+    public void setDelayMillis(CircleProgressView view, int val) {
         view.setDelayMillis(val);
     }
 
     @ReactProp(name = "fillColor")
-    public void setFillColor(CirclesView view, @Nullable String val) {
+    public void setFillColor(CircleProgressView view, @Nullable String val) {
         view.setFillCircleColor(Color.parseColor(val));
     }
 
     @ReactProp(name = "maxValue", defaultFloat = 0)
-    public void setMaxValue(CirclesView view, float val) {
+    public void setMaxValue(CircleProgressView view, float val) {
         view.setMaxValue(val);
     }
 
     @ReactProp(name = "value", defaultFloat = 0)
-    public void setValue(CirclesView view, float val) {
+    public void setValue(CircleProgressView view, float val) {
         if(_animated)
             view.setValueAnimated(val) ;
         else
@@ -109,62 +109,62 @@ public class CirclesManager extends SimpleViewManager<CirclesView> implements Ci
     }
 
     @ReactProp(name = "rimColor")
-    public void setRimColor(CirclesView view, @Nullable String val) {
+    public void setRimColor(CircleProgressView view, @Nullable String val) {
         view.setRimColor(Color.parseColor(val));
     }
 
     @ReactProp(name = "rimWidth", defaultInt = 0)
-    public void setRimWidth(CirclesView view, int val) {
+    public void setRimWidth(CircleProgressView view, int val) {
         view.setRimWidth(val);
     }
 
     @ReactProp(name = "startAngle", defaultInt = 0)
-    public void setStartAngle(CirclesView view, int val) {
+    public void setStartAngle(CircleProgressView view, int val) {
         view.setStartAngle(val);
     }
 
     @ReactProp(name = "unit")
-    public void setUnit(CirclesView view, @Nullable String val) {
+    public void setUnit(CircleProgressView view, @Nullable String val) {
         view.setUnit(val);
     }
 
     @ReactProp(name = "unitScale", defaultFloat = 0)
-    public void setUnitScale(CirclesView view, float val) {
+    public void setUnitScale(CircleProgressView view, float val) {
         view.setUnitScale(val);
     }
 
     @ReactProp(name = "unitVisible", defaultBoolean = false)
-    public void setUnitVisible(CirclesView view, boolean val) {
+    public void setUnitVisible(CircleProgressView view, boolean val) {
         view.setUnitVisible(val);
     }
 
     @ReactProp(name = "unitColor")
-    public void setUnitColor(CirclesView view, @Nullable String val) {
+    public void setUnitColor(CircleProgressView view, @Nullable String val) {
         view.setUnitColor(Color.parseColor(val));
     }
 
     @ReactProp(name = "unitSize", defaultInt = 0)
-    public void setUnitSize(CirclesView view, int val) {
+    public void setUnitSize(CircleProgressView view, int val) {
         view.setUnitSize(val);
     }
 
     @ReactProp(name = "textScale", defaultFloat = 0)
-    public void setTextScale(CirclesView view, float val) {
+    public void setTextScale(CircleProgressView view, float val) {
         view.setTextScale(val);
     }
 
     @ReactProp(name = "textSize", defaultInt = 0)
-    public void setTextSize(CirclesView view, int val) {
+    public void setTextSize(CircleProgressView view, int val) {
         view.setTextSize(val);
     }
 
     @ReactProp(name = "text")
-    public void setText(CirclesView view, @Nullable String val) {
+    public void setText(CircleProgressView view, @Nullable String val) {
         view.setText(val);
     }
 
     @ReactProp(name = "textMode")
-    public void setTextMode(CirclesView view, @Nullable String val) {
+    public void setTextMode(CircleProgressView view, @Nullable String val) {
         if(val.equalsIgnoreCase("text"))
             view.setTextMode(TextMode.TEXT);
         if(val.equalsIgnoreCase("value"))
@@ -174,27 +174,27 @@ public class CirclesManager extends SimpleViewManager<CirclesView> implements Ci
     }
 
     @ReactProp(name = "animated", defaultBoolean = false)
-    public void setAnimated(CirclesView view, boolean val) {
+    public void setAnimated(CircleProgressView view, boolean val) {
         _animated = val;
     }
 
     @ReactProp(name = "seekMode", defaultBoolean = false)
-    public void setSeekModeEnabled(CirclesView view, boolean val) {
+    public void setSeekModeEnabled(CircleProgressView view, boolean val) {
         view.setSeekModeEnabled(val);
     }
     
     @ReactProp(name = "spinColor")
-    public void setSpinColor(CirclesView view, @Nullable String val) {
+    public void setSpinColor(CircleProgressView view, @Nullable String val) {
         view.setSpinBarColor(Color.parseColor(val));
     }
 
     @ReactProp(name = "spinSpeed", defaultFloat = 10)
-    public void setSpinSpeed(CirclesView view, float val) {
+    public void setSpinSpeed(CircleProgressView view, float val) {
         view.setSpinSpeed(val);
     }
 
     @ReactProp(name = "spinBarLength", defaultFloat = 10)
-    public void setSpinBarLength(CirclesView view, float val) {
+    public void setSpinBarLength(CircleProgressView view, float val) {
         view.setSpinningBarLength(val);
     }
 
@@ -209,7 +209,7 @@ public class CirclesManager extends SimpleViewManager<CirclesView> implements Ci
 
     @Override
     public void receiveCommand(
-            CirclesView viewPager,
+            CircleProgressView viewPager,
             int commandType,
             @Nullable ReadableArray args) {
         Assertions.assertNotNull(viewPager);
